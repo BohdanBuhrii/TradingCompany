3ce88202790fd09b69f304db899a638d1edaf1e5
BohdanBuhrii <bobuhrii@gmail.com>
2019-09-28 20:07:22 +0300

    Starts with Dependency Injector

---
+++ DAL/DependencyInjectorDAL.cs
@@ -0,0 +1,37 @@
+﻿using DAL.Repositories.ImplementedRepositories;
+using DAL.Repositories.Interfaces;
+using Unity;
+
+namespace DAL
+{
+    public static class DependencyInjectorDAL
+    {
+        private readonly static IUnityContainer _unityContainer = GetUnity();
+
+        private static IUnityContainer GetUnity()
+        {
+            var container = new UnityContainer();
+            container.RegisterTypes();
+
+            return container;
+        }
+
+        private static void RegisterTypes(this IUnityContainer container)
+        {
+            container
+                .RegisterType<ICategoryGroupRepository, CategoryGroupRepository>()
+                .RegisterType<ICategoryRepository, CategoryRepository>()
+                .RegisterType<ICustomerRepository, CustomerRepository>()
+                .RegisterType<IDiscountRepository, DiscountRepository>()
+                .RegisterType<IManufacturerRepository, ManufacturerRepository>()
+                .RegisterType<IProductRepository, ProductRepository>()
+                .RegisterType<IRoleRepository, RoleRepository>()
+                .RegisterType<IUserRepository, UserRepository>();
+        }
+
+        public static T Resolve<T>()
+        {
+            return _unityContainer.Resolve<T>();
+        }
+    }
+}
