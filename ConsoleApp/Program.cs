ac273af391cdbc980355b40441fa297f5bc326f5
BohdanBuhrii <bobuhrii@gmail.com>
2019-11-03 19:15:26 +0200

    Add console startup

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using DAL;
+using DAL.Models;
+using DAL.UnitOfWork;
+using System;
+using System.Threading.Tasks;
+using TradingCompany.ConsoleApp.Pages;
+
+namespace TradingCompany
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //var unitOfWork = new UnitOfWork();
+
+            //var a = unitOfWork.CategoryRepository.GetByIdAsync(1).Result;
+            //var d = unitOfWork.CategoryGroupRepository.AddAsync(
+            //    new CategoryGroup 
+            //{ Name="knjkooooooooooook", IsActive=true }).Result;
+
+            //unitOfWork.SaveChanges();
+            //var a = await unitOfWork.ManufacturerRepository.GetAllAsync();
+
+            //unitOfWork.ManufacturerRepository.AddAsync(new Manufacturer { Name = "Olesya" });
+            //Console.WriteLine(unitOfWork.SaveChangesAsync());
+            //var a = await unitOfWork.ManufacturerRepository.GetByIdAsync(1);
+
+            //unitOfWork.ManufacturerRepository.Remove(a);
+
+            var page = new TablesPage();
+            page.Init();
+
+            //DateTime dateTime = new DateTime();
+            //string a = dateTime.ToString();//.ToShortDateString();
+            //Console.ReadLine();
+        }
+    }
+}
