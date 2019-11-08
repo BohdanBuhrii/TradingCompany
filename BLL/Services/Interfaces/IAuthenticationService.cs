4feb9691faefaefd98656b8fe9562763547f4486
BohdanBuhrii <bobuhrii@gmail.com>
2019-11-08 17:45:18 +0200

    Add service for authentication and related

---
+++ BLL/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using DTO;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> CheckCredentials(CredentialsDTO credentials);
+        Task<bool> UserExist(string login);
+    }
+}
