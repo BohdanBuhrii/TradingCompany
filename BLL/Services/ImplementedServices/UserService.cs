a74d0b722cc618c1ad865116c55c32a2034c460d
BohdanBuhrii <bobuhrii@gmail.com>
2019-11-11 21:56:12 +0200

    Start working with user

---
+++ BLL/Services/ImplementedServices/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using BLL.Services.Interfaces;
+using DTO;
+
+namespace BLL.Services.ImplementedServices
+{
+    public class UserService : IUserService
+    {
+        public Task<UserDTO> CreateUser(UserDTO user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserDTO> GetUserByEmail(string userEmail)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserDTO> GetUserById(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserDTO> UpdateUser(UserDTO user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BLL/Services/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using DTO;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDTO> GetUserById(int userId);
+
+        Task<UserDTO> GetUserByEmail(string userEmail);
+
+        Task<UserDTO> CreateUser(UserDTO user);
+
+        Task<UserDTO> UpdateUser(UserDTO user);
+
+        Task RemoveUser();
+    }
+}
