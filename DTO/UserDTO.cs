2a0ad2d88f7f41c10fb7fcf21a56687acea98007
BohdanBuhrii <bobuhrii@gmail.com>
2019-11-15 10:27:58 +0200

    Change UserDTO

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
 
         public RoleDTO Role { get; set; }
     }
