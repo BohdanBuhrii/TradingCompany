da6535f814a85a5a40151d666d2e2f93074d7086
BohdanBuhrii <bobuhrii@gmail.com>
2019-11-15 10:33:11 +0200

    Add config for mapper

---
+++ BLL/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DAL.Models;
+using DTO;
+
+namespace BLL
+{
+    public static class MapperConfig
+    {
+        public static IMapper CreateMapper()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDTO>();
+                cfg.CreateMap<Role, RoleDTO>();
+                cfg.CreateMap<RoleDTO, Role>()
+                    .ForMember(r => r.Users, opt => opt.Ignore());
+            }).CreateMapper();
+        }
+    }
+}
